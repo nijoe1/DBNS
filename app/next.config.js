82aca38fb243158c44342f9be019de86cce1815e
nick-w3 <nicklioniss@gmail.com>
2024-03-03 22:40:42 +0200

    Fleek config

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 
   webpack: (config) => {
     config.resolve = {
